2a1d00bf807d6e6557f30caf07f6f960f4595b49
Paulo Aboim Pinto <aboimpinto@gmail.com>
2021-05-06 00:06:36 +0200

    initial commit

---
+++ Avalonia.UITestFramework.Example/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalonia.UITestFramework.Example.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ Avalonia.UITestFramework/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Avalonia.UITestFramework
+{
+    public class Class1
+    {
+    }
+}
